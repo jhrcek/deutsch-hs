757c2f74bbfc59b08cf1ea172d8ec35f06d24cc4
Jan Hrcek <jhrcek@redhat.com>
2015-01-01 11:57:35 +0100

    WordData made data type insteady of alias for [(Text, Text)]

--- dwds.hs
+++ dwds.hs
@@ -6,7 +6,7 @@ import Control.Monad (forM, when)
 import Control.Monad.Trans.Control (MonadBaseControl)
 import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (catMaybes)
-import Data.Text (Text, concat, splitOn, append, intercalate)
+import Data.Text (Text, concat, splitOn, append, intercalate, pack)
 import qualified Data.Text.IO as T
 import Prelude hiding (concat)
 import Test.WebDriver (WDConfig, defaultConfig, defaultCaps, wdCapabilities, browser, chrome, runSession,finallyClose, WD, findElem, findElems, getText, Element, attr, openPage, click, clearInput, sendKeys, Selector(..), FailedCommandType(..), FailedCommand(..))
@@ -25,22 +25,25 @@ main = runSession myConfig . finallyClose $ do
    liftIO . mapM_ (T.putStrLn . formatWordData) $ wordData
 
 formatWordData :: WordData -> Text
-formatWordData (("word", w) : wdata) = w `append` 
-  case wdata of
-    []        -> " NOT FOUND"
-    otherwise -> concat $ map (\(field, value) -> concat ["\n  ",field, " = ", value]) wdata
+formatWordData Word {wWord = w, wPronUrls = prons, wDefinitions = defs} = w `append`
+  if all null [prons, defs]
+    then " NOT FOUND"
+    else fmt "pron" prons `append` fmt "def" defs
+    where fmt label items = concat $ zipWith (\ item index -> concat ["\n  ", label, index, " = ", item]) items textIdxs
+          textIdxs = map (pack . show) [1..]
  
 extractWordData :: Text -> WD WordData
 extractWordData word = do
-  found <- search word
-  extract <- if found then do
-      definition <- zip (repeat "definition") <$> getDefinitions
-      urls <- zip (repeat "url") <$> getPronUrls
-      return $ definition ++ urls
-    else return []
-  return $ ("word", word) : extract
+  search word
+  defs <- getDefinitions
+  pronUrls <- getPronUrls
+  return Word {wWord = word, wPronUrls = pronUrls, wDefinitions = defs}
 
-type WordData = [(Text, Text)]
+data WordData = Word 
+    { wWord :: Text
+    , wPronUrls :: [Text]
+    , wDefinitions :: [Text]
+    }
 
 getDefinitions :: WD [Text]
 getDefinitions = findElems definition >>= mapM getText
@@ -67,20 +70,19 @@ waitPanelsLoaded :: WD ()
 waitPanelsLoaded = waitWhile 5 $ findElem loadingAnimation
 
 
-search :: Text -> WD Bool
+search :: Text -> WD ()
 search str = do
   inp <- findElem searchInput
   clearInput inp
   sendKeys str inp
   findElem searchButton >>= click
   waitPanelsLoaded
-  null <$> findElems notFoundMessage
 
 
-definition, loadingAnimation, notFoundMessage, pronPlayer, searchButton, searchInput :: Selector
+definition, loadingAnimation, {-notFoundMessage,-} pronPlayer, searchButton, searchInput :: Selector
 definition       = ByCSS "div[id^=sense]>.wb_bp"
 loadingAnimation = ByClass "panel_loading"
-notFoundMessage  = ByXPath "//p[contains(text(),'Kein Eintrag vorhanden')]"
+--notFoundMessage  = ByXPath "//p[contains(text(),'Kein Eintrag vorhanden')]"
 pronPlayer       = ById "oneBitInsert_1"
 searchButton     = ById "dwds_main_search_submit"
 searchInput      = ById "query_fast_search"
